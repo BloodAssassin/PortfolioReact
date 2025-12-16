9bfdc1428b8be4bc2c260b01e3e13222b11a4ef0
Milos Ristic <rmilos65@gmail.com>
2025-12-16 23:45:14 +0100

    Footer added.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Contact from "./pages/Contact/Contact";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,59 @@
+import styles from "./Footer.module.scss";
+import { socials } from "../../data/socials";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={`content ${styles.content}`}>
+        <h3 className={styles.branding}>Got a Project in Mind? Let's Talk :)</h3>
+
+        <form
+          className={styles.contactForm}
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <div>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Name"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div>
+            <textarea
+              id="message"
+              name="message"
+              rows={4}
+              placeholder="Message"
+              required
+            />
+          </div>
+          <button type="submit">Send</button>
+        </form>
+
+        <ul className={styles.socials}>
+          {socials.map((social) => (
+            <li key={social.name}>
+              <a href={social.url} target="_blank" rel="noopener noreferrer">
+                <img src={social.icon ?? ""} alt={social.name} />
+                {social.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
